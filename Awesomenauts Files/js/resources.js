74aee7ec27a7319dbe65939eacab214c1a0aa8d5
Kenneth Prado <Kennethprado45@gmail.com>
2015-01-23 16:41:43 -0800

    Game Project Video 2 comments. The first push was the code without the comments and this one is the comments with the code.

--- Awesomenauts Files/js/resources.js
+++ Awesomenauts Files/js/resources.js
@@ -5,18 +5,21 @@ game.resources = [
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
 	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 //The background tiles for the game will create tiles on tiled.
 	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	 //The meta tiles work as solid tiles to make specific parts of the game solid.
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
-		
+	
 	/* Maps. 
 	 * @example
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
  	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+ 	 //This part for level01 will run the game on the webpage.
 
 	/* Background music. 
 	 * @example
