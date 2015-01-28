9861f16c38e1b30acc53ce2e3ef66824176c4775
Kenneth Prado <Kennethprado45@gmail.com>
2015-01-28 12:04:42 -0800

    Game Project So I had to delete most of the files and start over, however I am on video 7.

--- js/resources.js
+++ js/resources.js
@@ -5,23 +5,22 @@ game.resources = [
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
 	 {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-	 //The background tiles for the game will create tiles on tiled.
 	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-	 //The meta tiles work as solid tiles to make specific parts of the game solid.
 	 {name: "player", type:"image", src: "data/img/orcSpear.png"},
-	 //The player will show because of this code.
+
+
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
-	
+		
 	/* Maps. 
 	 * @example
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
  	 {name: "level01", type: "tmx", src: "data/map/level01.tmx"},
- 	 //This part for level01 will run the game on the webpage.
+
 
 	/* Background music. 
 	 * @example
