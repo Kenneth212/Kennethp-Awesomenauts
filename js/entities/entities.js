760d942465d3c218c2a7be9c10978a2e4105545e
Kenneth Prado <Kennethprado45@gmail.com>
2015-02-13 10:32:59 -0800

    Game Project Video 22 code

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,6 +10,8 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
+		this.type ="PlayerEntity";
+		this.health = 20;
 		this.body.setVelocity(5, 20);
 		//Keeps track of which direction your charcter is going
 		this.facing = "right";
@@ -82,6 +84,11 @@ game.PlayerEntity = me.Entity.extend({
 		
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+		console.log(this.health);
 	},
 
 	collideHandler: function(response) {
@@ -261,6 +268,25 @@ game.EnemyBaseEntity = me.Entity.extend({
 				this.pos.x = this.pos.x + 1;
 				//checks that it has been at least 1 second since this creep hit a base
 				if((this.now-this.lastHit >=1000)){
+					//updates th lastHit timer 
+					this.lastHit = this.now;
+					//makes the player base call its loseHealth function and passes it a 
+					//damage of 1 
+					response.b.loseHealth(1);
+				}
+			}else if (response.b.type==='PlayerEntity'){
+				var xdif = this.pos.x - response.b.pos.x;
+
+				this.attacking=true;
+				//this.lastAttacking=this.now;
+				this.body.vel.x = 0;
+				//keeps moving the creep to the right to maintain its position
+				if(xdif>0){
+					this.pos.x = this.pos.x + 1;
+					this.body.vel.x = 0;
+				}
+				//checks that it has been at least 1 second since this creep hit a base
+				if((this.now-this.lastHit >=1000) && xdif>0){
 					//updates th lastHit timer 
 					this.lastHit = this.now;
 					//makes the player base call its loseHealth function and passes it a 
