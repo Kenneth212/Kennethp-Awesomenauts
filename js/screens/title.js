fc9643993f9268e67e42cbfffffe00351d1e206d
Kenneth Prado <Kennethprado45@gmail.com>
2015-02-20 18:26:17 -0800

    Game Project Video 26 comments.

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,7 +5,8 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 	},
-	
+	//this will make the title show up, unfortunately it will not go into your game so you wont 
+	//be able to use the game till the title is functional
 	
 	
 	/**	
