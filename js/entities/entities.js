df62d5dca564081b2dee5e1ee6bfdfccd1480a7d
Kenneth Prado <Kennethprado45@gmail.com>
2015-02-27 12:09:32 -0800

    Game Project Video 30 code.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,5 +1,21 @@
 game.PlayerEntity = me.Entity.extend({
 	init: function(x, y, settings) {
+		this.setSuper();
+		this.setPlayerTimers();
+		this.setAttributes();
+		this.type ="PlayerEntity";
+		this.setFlags();
+		//this shows how mush health we have
+		//the health can be changed
+		//this.dead will show us if if its false that the player is dead(only shows up if its false)
+		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+
+		this.addAnimation():
+
+		this.renderable.setCurrentAnimation("idle");
+	},
+
+	setSuper: function () {
 		this._super(me.Entity, "init", [x, y, {
 			image: "player",
 			width: 64,
@@ -10,27 +26,30 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
-		this.type ="PlayerEntity";
-		this.health = game.data.playerHealth;
-		//this shows how mush health we have
-		//the health can be changed
-		this.body.setVelocity(game.data.playerMoveSpeed, 20);
-		//Keeps track of which direction your charcter is going
-		this.facing = "right";
+	},
+
+	setPlayerTimers: function() {
 		this.now = new Date().getTime();
 		this.lastHit = this.now;
-		this.dead = false;
-		//this.dead will show us if if its false that the player is dead(only shows up if its false)
-		this.attack = game.data.playerAttack;
 		this.lastAttack = new Date().getTime();
-		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+	},
 
+	setAttributes: function(){
+		this.health = game.data.playerHealth;
+		this.body.setVelocity(game.data.playerMoveSpeed, 20);
+		this.attack = game.data.playerAttack;
+	},
+
+	setFlags: function() {
+		//Keeps track of which direction your charcter is going
+		this.facing = "right";
+		this.dead = false;
+	},
+
+	addAnimation: function() {
 		this.renderable.addAnimation("idle", [78]);
 		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
 		this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
-
-
-		this.renderable.setCurrentAnimation("idle");
 	},
 
 	update: function(delta) {
@@ -163,9 +182,7 @@ game.PlayerEntity = me.Entity.extend({
 		}
 	}
 });
-//The game.PlayerBaseEntity
 
-//The game.EnemyBaseEntity will create the enemy base
 
 
 
