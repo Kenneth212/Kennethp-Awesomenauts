477ca661c84ac7dc6eab74735d3a37f9dc15366d
Kenneth Prado <Kennethprado45@gmail.com>
2015-03-05 20:29:06 -0800

    Game Project Video 39 code.

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,28 +6,32 @@ game.TitleScreen = me.ScreenObject.extend({
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 	
 
-		me.input.bindKey(me.input.KEY.ENTER, "start");
 		//this line of code will make the enter button be pressed and show up the game.
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function() {
-				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				this.font = new me.Font("Arial", 46, "white");
+				me.input.register.PointerEvent("pointerdown", this, this.newGame.bind(this), true)
 			},
 			//The code here will make an enter button show up and to make the game functional.
 			draw: function(renderer) {
-				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-				this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
-			}
-		})));
+				this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+			},
 
-		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-			if(action === "start"){
+			update: function(dt){
+				return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.save.remove('exp');
+				me.save.remove('exp1');
+				me.save.remove('exp2');
+				me.save.remove('exp3');
+				me.save.remove('exp4');
 				me.state.change(me.state.PLAY);
-			}
-	});
-
-
-
+			}	
+		})));
 
 	},
 	//this will make the title show up, unfortunately it will not go into your game so you wont 
@@ -38,7 +42,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKey(me.input.KEY.ENTER); // TODO
-		me.event.unsubscribe(this.handler);
+
 	}
 });
