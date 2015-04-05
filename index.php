797360f0e50b1a6ae195962419c5fdac6e708981
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-05 11:39:05 -0700

    Game Project Video 60 code.

--- index.php
+++ index.php
@@ -57,6 +57,7 @@
 		<script type="text/javascript" src="js/gamemanagers/HeroDeathManager.js"></script>
 		<script type="text/javascript" src="js/entities/PlayerBaseEntity.js"></script>
 		<script type="text/javascript" src="js/entities/HUD.js"></script>
+		<script type="text/javascript" src="js/entities/SpearThrow.js"></script>
 		<script type="text/javascript" src="js/screens/title.js"></script>
 		<script type="text/javascript" src="js/screens/play.js"></script>
 		<script type="text/javascript" src="js/screens/spendExp.js"></script>
--- js/screens/title.js
+++ js/screens/title.js
@@ -5,9 +5,9 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 	
-
+		game.data.option1 = new (me.Renderable.extend({
 		//this line of code will make the enter button be pressed and show up the game.
-		me.game.world.addChild(new (me.Renderable.extend({
+		
 			init: function() {
 				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 				this.font = new me.Font("Arial", 46, "white");
@@ -26,6 +26,13 @@ game.TitleScreen = me.ScreenObject.extend({
 			//the experience
 			newGame: function(){
 				me.input.releasePointerEvent('pointerdown', this);
+				me.input.releasePointerEvent('pointerdown', game.data.option2);
+				me.save.remove('exp');
+				me.save.remove('exp1');
+				me.save.remove('exp2');
+				me.save.remove('exp3');
+				me.save.remove('exp4');
+				me.save.add({exp: 0,exp: 1,exp: 2,exp: 3,exp: 4,})
 				me.state.change(me.state.NEW);
 			}	
 		})));
@@ -33,6 +40,8 @@ game.TitleScreen = me.ScreenObject.extend({
 		//this line of code will make the enter button be pressed and show up the game
 		//this code was copyed and some code was removed
 		me.game.world.addChild(new (me.Renderable.extend({
+
+		game.data.option2 = new (me.Renderable.extend({
 			init: function() {
 				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
 				this.font = new me.Font("Arial", 46, "white");
@@ -49,15 +58,22 @@ game.TitleScreen = me.ScreenObject.extend({
 			//the new game function contains some specific parts for
 			//the experience
 			newGame: function(){
+				game.data.exp = me.save.exp;
+				game.data.exp1 = me.save.exp1;
+				game.data.exp2 = me.save.exp2;
+				game.data.exp3 = me.save.exp3;
+				game.data.exp4 = me.save.exp4;
+				me.input.releasePointerEvent('pointerdown', game.data.option1);
 				me.input.releasePointerEvent('pointerdown', this);
 				me.state.change(me.state.LOAD);
 			//so this will make the continue button be pressed and head over to the spend page
 			}	
-		})));
+		}));
 
+	
+		me.game.world.addChild(game.data.option2);
 	},
-	//this will make the title show up, unfortunately it will not go into your game so you wont 
-	//be able to use the game till the title is functional
+
 	
 	
 	/**	
