c3656899abe691ffed7486fcb1c74fa43a2cd4ec
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-06 22:04:57 -0700

    Game Project Video 64 code.

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,5 +1,16 @@
 game.MiniMap = me.Entity.extend({
-
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap",
+			width: "1014",
+h			height: "262",
+			spritewidth: "1014",
+			spriteheight: "262",
+			getShape: function(){
+				return (new me.Rect(0, 0, 1014, 262)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+	}
 });
 
-	
